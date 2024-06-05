1fe906fafa08d61aef57d623740703defeceb222
RaquelLores <rlorescasalinas@gmail.com>
2024-06-05 17:01:47 +0200

    scaffolding-app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+    <Router>
+      <div className="main_section w-full min-h-screen scroll-smooth">This is the main section</div>
+    </Router>
+    </>
+  );
+}
+
+export default App;
