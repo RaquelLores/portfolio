87537c199fe78fce0719642f65c7226f37dbac31
RaquelLores <rlorescasalinas@gmail.com>
2024-06-21 16:28:05 +0200

    header-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
-import Header from "./components/header/Header.jsx"
+import Header from './components/header/Header.jsx';
+ 
 
 function App() {
 
