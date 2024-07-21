d356c32eeda02d97970bfe265252be42cb53b108
RaquelLores <rlorescasalinas@gmail.com>
2024-07-21 10:22:02 +0200

    plan_home

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./home/Home.css";
+import { Link } from 'react-router-dom';
+
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero_img lg:px-32 md:px-16 sm:px-10 px-10 relative">
+        <div className="lg:w-2/4 md:w-2/3 sm:w-full w-full lg:mt-8 md:mt-5 sm:mt-1 mt-0">
+          <p className="text-gray-400 lg:text-2xl md:text-xl sm:text-xl text-xl font-medium tracking-[0.05rem] mb-4">
+            Meet a Front-End Developer 👩🏻‍💻,
+          </p>
+          <h1 className="text-slate-100.lg:text-8xl.md:text-6xl.text-5xl.text-5xl.font-black.lg:leading">
+            Raquel Lores Casalinas
+          </h1>
+        </div>
+      </div >
+    </>
+  );
+};
+export default Home;
