21a58ec23272ee8080ad32f292681b6fc6a7e3d0
RaquelLores <rlorescasalinas@gmail.com>
2024-07-24 17:41:14 +0200

    add_content_home

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/header/Header.jsx';
- 
+import Home from './components/home/Home.jsx';
+
 
 function App() {
 
   return (
     <>
-    <Router>
-      <div className="main_section w-full min-h-screen scroll-smooth">This is the main section
-      <Header/>
-      </div>
-    </Router>
+      <Router>
+        <Header />
+        <div className="main_section w-full min-h-screen scroll-smooth">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+
     </>
   );
 }
